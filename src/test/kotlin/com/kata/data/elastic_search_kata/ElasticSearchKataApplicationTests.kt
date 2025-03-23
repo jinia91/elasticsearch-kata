8076f52930538503d4108b5386ae99bee8d760fb
jinia91 <creee77@gmail.com>
2025-03-23 12:31:16 +0900

    elastic search 기본 api 학습

---
+++ src/test/kotlin/com/kata/data/elastic_search_kata/ElasticSearchKataApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.kata.data.elastic_search_kata
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ElasticSearchKataApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
